239c19e848589426263b7689cf8fda43ad84756d
Ashley <ashleywong678@gmail.com>
2019-12-29 09:55:49 -0500

    removed unneeded components and made test navbar

--- app/components/Header/NavBar.js
+++ app/components/Header/NavBar.js
@@ -1,5 +1,25 @@
+import React from 'react';
+
 import styled from 'styled-components';
 
-export default styled.div`
-  text-align: center;
+const NavBarWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 10;
+  gridcolumn: 1 / end;
+  height: 70px;
+  background-color: blue;
 `;
+
+const Name = styled.h1`
+  color: black;
+`;
+
+function NavBar() {
+  return (
+    <NavBarWrapper>
+      <Name>Hello</Name>
+    </NavBarWrapper>
+  );
+}
+
+export default NavBar;
--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -5,18 +5,14 @@
  * This component is the skeleton around the actual pages, and should only
  * contain code that should be seen on all pages. (e.g. navigation bar)
  */
-
-import React, { useEffect, useCallback, memo } from 'react';
+// { useEffect, useCallback, memo }
+import React from 'react';
 // import { Helmet } from 'react-helmet';
 import styled from 'styled-components';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import HomePage from 'containers/HomePage/Loadable';
-import FeaturePage from 'containers/FeaturePage/Loadable';
-import NotFoundPage from 'containers/NotFoundPage/Loadable';
 import Header from 'components/Header';
-import Footer from 'components/Footer';
-
 import GlobalStyle from '../../global-styles';
 
 const AppWrapper = styled.div`
@@ -31,19 +27,10 @@ const AppWrapper = styled.div`
 export default function App() {
   return (
     <AppWrapper>
-      <Helmet
-        titleTemplate="%s - React.js Boilerplate"
-        defaultTitle="React.js Boilerplate"
-      >
-        <meta name="description" content="A React.js Boilerplate application" />
-      </Helmet>
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/features" component={FeaturePage} />
-        <Route path="" component={NotFoundPage} />
       </Switch>
-      <Footer />
       <GlobalStyle />
     </AppWrapper>
   );
